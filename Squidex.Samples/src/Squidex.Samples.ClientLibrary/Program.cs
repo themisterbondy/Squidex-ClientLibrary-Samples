b3112d825819cb8cd3aa121fa44a5ea075370a88
Alexandre Nascimento <alexandre.n@outlook.com>
2019-04-11 14:05:27 -0300

    Create Project

---
+++ Squidex.Samples/src/Squidex.Samples.ClientLibrary/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Squidex.Samples.ClientLibrary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
